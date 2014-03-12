779364fbdaf598d4dab5b24ebf64bd28e84f603a
Davide Nunes <davex.pt@gmail.com>
2014-03-12 18:23:09 +0000

    first commit for the posthoc branch

---
+++ src/main/java/org/bhave/experiment/data/posthoc/PostHocStatistics.java
@@ -0,0 +1,66 @@
+package org.bhave.experiment.data.posthoc;
+
+import java.util.List;
+import java.util.Properties;
+
+import org.bhave.experiment.Model;
+
+/**
+ * <p>
+ * PostHocStatistics are measurements that can be made after the model has been
+ * executed. These measurements, reports or descriptions do not need information
+ * from the modelo while this is running, as such they do not need to be
+ * integrated in a producer - consumer environment. Instead we take the model,
+ * generate the data, and store it directly with the appropriate DataExporter.
+ * </p>
+ * 
+ * @author Davide Nunes
+ * 
+ */
+public interface PostHocStatistics {
+	/**
+	 * Returns the name of this statistics measure or report.
+	 * 
+	 * @return
+	 */
+	public String getName();
+
+	/**
+	 * This is used to verify if the data statistics class was designed for the
+	 * model being measured. This means that when you implement this class you
+	 * must provide the target model class.
+	 * 
+	 * @return a Model Class a model being measured by this statistics
+	 * @see Model
+	 * 
+	 */
+	public Class<? extends Model> getTargetModelClass();
+
+	/**
+	 * <p>
+	 * Analyses a model and returns a list of properties. This is basically a
+	 * table and the list provives the multiple rows of the table.
+	 * </p>
+	 * 
+	 * <p>
+	 * For each element in the table you can access the relevant column using
+	 * the keys from the Properties object ({@link Properties#propertyNames()});
+	 * </p>
+	 * 
+	 * @param model
+	 *            the model being measured
+	 * @return a list of {@link Properties} with the measurements for the model
+	 */
+	public List<Properties> measure(Model model);
+
+	/**
+	 * <p>
+	 * Retuns a list of the names for the columns that contain data, while the
+	 * order is not important, the list should correspond to keys on each item
+	 * in the list returned by {@link #measure(Model)}.
+	 * </p>
+	 * 
+	 * @return a list of names for the data columns.
+	 */
+	public List<String> getDataColumns();
+}
